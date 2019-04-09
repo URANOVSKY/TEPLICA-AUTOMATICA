bdf46c3b08e6a736a86c2d7d8d3850550cec86d7
URANOVSKY <48415040+URANOVSKY@users.noreply.github.com>
2019-04-09 23:02:01 +0300

    Доработки счётчика

--- WEB/scripts/counter.js
+++ WEB/scripts/counter.js
@@ -4,14 +4,17 @@ var counter = 27.5
 $('#counter').html(counter)
 
 $('#minus').click(() => {
-  counter = counter - 0.5
+  counter = counter - 0.5;
+  if (counter < 20) counter = 35;
   $('#counter').html(counter)
 });
 
 $('#plus').click(() => {
-  counter = counter + 0.5
+  counter = counter + 0.5;
+  if (counter > 35) counter = 20;
   $('#counter').html(counter)
 });
+
 
 //extra: SPACE to reset counter
 //change colors if below or above -10/10 i.e.
